dbf0912bc05edf77e639f80ca0bae84645d9f38b
Amarendra Rout <amarendrarout34@gmail.com>
2023-02-10 01:13:20 +0530

    useraddress

--- frontend/src/Component/Homepage/pages/User/UserAddress/UserAddress.js
+++ frontend/src/Component/Homepage/pages/User/UserAddress/UserAddress.js
@@ -14,23 +14,32 @@ const UserAddress = () => {
                 ]}
             /> */}
             <div className="container mt-2">
-                <div className="d-flex justify-content-start align-items-center gap-5 mb-4">
+                <div className="d-none text-center m-5 p-5">
+                    <p>You don't have any saved address yet.</p>
+                    <a href="/" className="btn btn-md btn-outline-primary" data-bs-toggle="modal" data-bs-target="#addAddressModal">
+                        <i class="bi bi-plus-lg" /> Add address
+                    </a>
+                </div>
+                <div className="d-flex justify-content-between align-items-center gap-5 mb-4">
                     <h4 className="mb-">Address</h4>
                     <a href="/" className="btn btn-md btn-outline-primary" data-bs-toggle="modal" data-bs-target="#addAddressModal">
-                        <i class="bi bi-plus-lg"/> Add address
+                        <i class="bi bi-plus-lg" /> Add address
                     </a>
                 </div>
                 <div className="row">
                     <div className="col-lg-5 col-xxl-4 col-12 mb-4">
                         <div className="card border-info">
-                            <div className="card-header bg-info-subtle border-0 p-0 m-0 px-2 py-2">
-                                <span className="h6">Home</span>
+                        <div className="card-header bg-info-subtle d-flex flex-0 justify-content-between align-items-center p-1 m-0">
+                                <span className="h6 mt-2">Home</span>
+                                <a href="/" className="btn btn-sm disabled bg-light btn-outline-light text-muted rounded rounded-3" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                    Set default
+                                </a>
                             </div>
                             <div className="card-body p-2">
                                 <p className="mb-2 fw-semibold">Amarendra Rout</p>
                                 <p className="mb-1">
-                                    Plot no 1520, Snow House,<br/>
-                                    CDA Sector-6, Cuttack, Odisha<br/>
+                                    Plot no 1520, Snow House,<br />
+                                    CDA Sector-6, Cuttack, Odisha<br />
                                     INDIA, 753006
                                 </p>
                                 <p className="mb-1">+91 7043096106</p>
@@ -48,14 +57,17 @@ const UserAddress = () => {
                     </div>
                     <div className="col-lg-5 col-xxl-4 col-12 mb-4">
                         <div className="card">
-                            <div className="card-header bg-light-subtle border-0 p-0 m-0 px-2 py-2">
-                                <span className="h6">Office</span>
+                            <div className="card-header bg-light-subtle d-flex flex-0 justify-content-between align-items-center p-1 m-0">
+                                <span className="h6 mt-2">Office</span>
+                                <a href="/" className="btn btn-sm btn-outline-info rounded rounded-3" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                    Set default
+                                </a>
                             </div>
                             <div className="card-body p-2">
                                 <p className="mb-2 fw-semibold">Amarendra Rout</p>
                                 <p className="mb-1">
-                                    Plot no 1520, Snow House,<br/>
-                                    CDA Sector-6, Cuttack, Odisha<br/>
+                                    Plot no 1520, Snow House,<br />
+                                    CDA Sector-6, Cuttack, Odisha<br />
                                     INDIA, 753006
                                 </p>
                                 <p className="mb-1">+91 7043096106</p>
@@ -73,14 +85,17 @@ const UserAddress = () => {
                     </div>
                     <div className="col-lg-5 col-xxl-4 col-12 mb-4">
                         <div className="card">
-                            <div className="card-header bg-light-subtle border-0 p-0 m-0 px-2 py-2">
-                                <span className="h6">Other</span>
+                        <div className="card-header bg-light-subtle d-flex flex-0 justify-content-between align-items-center p-1 m-0">
+                                <span className="h6 mt-2">Other</span>
+                                <a href="/" className="btn btn-sm btn-outline-info rounded rounded-3" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                    Set default
+                                </a>
                             </div>
                             <div className="card-body p-2">
                                 <p className="mb-2 fw-semibold">Amarendra Rout</p>
                                 <p className="mb-1">
-                                    Plot no 1520, Snow House,<br/>
-                                    CDA Sector-6, Cuttack, Odisha<br/>
+                                    Plot no 1520, Snow House,<br />
+                                    CDA Sector-6, Cuttack, Odisha<br />
                                     INDIA, 753006
                                 </p>
                                 <p className="mb-1">+91 7043096106</p>
