d37763777ab5fe5f92f0fc4a5ac9631d38327300
Prakash <prakash.rout14@gmail.com>
2023-02-19 15:20:03 +0530

    1stcommit

--- frontend/src/Component/Homepage/pages/Home/HomeCategory/HomeCategory.js
+++ frontend/src/Component/Homepage/pages/Home/HomeCategory/HomeCategory.js
@@ -7,7 +7,7 @@ const HomeCategory = () => {
 
     const navigate = useNavigate();
 
-    const clientURL = process.env.REACT_APP_CLIENT_URL;
+    // const clientURL = process.env.REACT_APP_CLIENT_URL;
 
     return (
 
@@ -19,32 +19,32 @@ const HomeCategory = () => {
                 </h4>
                 <div className="text-center d-flex justify-content-center row row-cols-2 row-cols-sm-3 row-cols-md-3 row-cols-lg-4 g-2 g-sm-2 g-md-3 g-lg-4">
                     <button className="col btn btn-default cat-btn" onClick={() => navigate('/category/kurtis')}>
-                        <img src={clientURL + "/assets/images/categoryImages/kurti.jpg"} className="category-img bd-placeholder-img rounded-circle border " alt="" />
+                        <img src= "/assets/images/categoryImages/kurti.jpg" className="category-img bd-placeholder-img rounded-circle border " alt="" />
                         <h6 className="fw-normal pt-2 category-text">Kurtis</h6>
                     </button>
                     <button className="col btn btn-default cat-btn" onClick={() => navigate('/category/dupattas')}>
-                        <img src={clientURL + "/assets/images/catImages/dupattas.jpg"} className="category-img bd-placeholder-img rounded-circle border " alt="" />
+                        <img src="/assets/images/catImages/dupattas.jpg" className="category-img bd-placeholder-img rounded-circle border " alt="" />
                         <h6 className="fw-normal pt-2 category-text">Dupattas</h6>
                     </button>
                     <button className="col btn btn-default cat-btn" onClick={() => navigate('/category/dress')}>
-                        <img src={clientURL + "/assets/images/catImages/dress.jpg"} className="category-img bd-placeholder-img rounded-circle border " alt="" />
+                        <img src="/assets/images/catImages/dress.jpg" className="category-img bd-placeholder-img rounded-circle border " alt="" />
                         <h6 className="fw-normal pt-2 category-text">Dress</h6>
                     </button>
                     <button className="col btn btn-default cat-btn" onClick={() => navigate('/category/fabrics')}>
-                        <img src={clientURL + "/assets/images/catImages/fabrics.jpg"} className="category-img bd-placeholder-img rounded-circle border " alt="" />
+                        <img src="/assets/images/catImages/fabrics.jpg" className="category-img bd-placeholder-img rounded-circle border " alt="" />
                         <h6 className="fw-normal pt-2 category-text">Fabrics</h6>
                     </button>
                     <button className="col btn btn-default cat-btn" onClick={() => navigate('/category/dress')}>
-                        <img src={clientURL + "/assets/images/catImages/pants.jpg"} className="category-img bd-placeholder-img rounded-circle border " alt="" />
+                        <img src="/assets/images/catImages/pants.jpg" className="category-img bd-placeholder-img rounded-circle border " alt="" />
                         <h6 className="fw-normal pt-2 category-text">Pants / Palazzo</h6>
                     </button>
                     <button className="col btn btn-default cat-btn" onClick={() => navigate('/category/jewellery')}>
                         {/* <img src="http://localhost:3000/assets/images/categoryImages/jewellery.png" className="category-img bd-placeholder-img rounded-circle border " alt="" /> */}
-                        <img src={clientURL + "/assets/images/catImages/jewellery.jpg"} className="category-img bd-placeholder-img rounded-circle border " alt="" />
+                        <img src="/assets/images/catImages/jewellery.jpg" className="category-img bd-placeholder-img rounded-circle border " alt="" />
                         <h6 className="fw-normal pt-2 category-text">Jewellery</h6>
                     </button>
                     <button className="col btn btn-default cat-btn opacity-1" onClick={() => navigate('/category/others')}>
-                        <img src={clientURL + "/assets/images/catImages/others.jpg"} className="category-img bd-placeholder-img rounded-circle border " alt="" />
+                        <img src="/assets/images/catImages/others.jpg" className="category-img bd-placeholder-img rounded-circle border " alt="" />
                         <h6 className="fw-normal pt-2 category-text">Other Accessories</h6>
                     </button>
                 </div>
