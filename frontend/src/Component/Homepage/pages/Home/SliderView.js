58c31ac2da5d16df71832b4947f480f84b6a6e26
Amarendra Rout <amarendrarout34@gmail.com>
2023-04-01 00:39:43 +0530

    01-04-2023>00:00

--- frontend/src/Component/Homepage/pages/Home/SliderView.js
+++ frontend/src/Component/Homepage/pages/Home/SliderView.js
@@ -85,16 +85,16 @@ const SliderView = () => {
 
     return (
         // <div className="my-2">
-        <div id="carouselExampleControls" className="carousel" data-bs-ride="carousel">
-            <div class="carousel-indicators">
+        <div id="carouselExampleControls" className="carousel bg-light-subtle" data-bs-ride="carousel">
+            {/* <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleControls" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleControls" data-bs-slide-to="1" aria-label="Slide 3"></button>
                 <button type="button" data-bs-target="#carouselExampleControls" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 <button type="button" data-bs-target="#carouselExampleControls" data-bs-slide-to="3" aria-label="Slide 4"></button>
-            </div>
+            </div> */}
             <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <div className="card">
+                {/* <div className="carousel-item active">
+                    <div className="card" style={{ maxHeight: '500px' }}> */}
                         {/* <div className="img-wrapper"><img src="..." className="d-block w-100" alt="..."> </div> */}
                         {/* <div className="img-wrapper">
                                 <img src="/assets/images/bannerImages/banner-1.jpeg" className="d-block w-100" alt="product" />
@@ -116,7 +116,7 @@ const SliderView = () => {
                                 <p className="fs-6 fw-semibold text-start">Limited quantities.</p>
                             </div>
                         </div> */}
-                        <div className='row bg-warning-subtle'>
+                        {/* <div className='row bg-warning-subtle'>
                             <div className='col-12 col-md-6'>
                                 <div className='py-5 px-3'>
                                     <p className="fs-5 fw-bold lh-1 ls-1 text-muted"><small>Trade in offer</small></p>
@@ -131,9 +131,9 @@ const SliderView = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="card">
+                </div> */}
+                {/* <div className="carousel-item">
+                    <div className="card" style={{ maxHeight: '500px' }}> */}
                         {/* <div className="img-wrapper"><img src="..." className="d-block w-100" alt="..."> </div> */}
                         {/* <div className="img-wrapper">
                                 <img src="/assets/images/bannerImages/banner-1.jpeg" className="d-block w-100" alt="product" />
@@ -151,7 +151,7 @@ const SliderView = () => {
                                 <p className="fs-6 fw-semibold">Limited quantities.</p>
                             </div>
                         </div> */}
-                        <div className='row bg-danger-subtle'>
+                        {/* <div className='row bg-danger-subtle'>
                             <div className='col-12 col-md-6'>
                                 <div className='py-5 px-3'>
                                     <p className="fs-5 fw-bold lh-1 ls-1 text-muted"><small>Trade in offer</small></p>
@@ -166,9 +166,9 @@ const SliderView = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="card">
+                </div> */}
+                {/* <div className="carousel-item">
+                    <div className="card" style={{ maxHeight: '500px' }}> */}
                         {/* <div className="img-wrapper"><img src="..." className="d-block w-100" alt="..."> </div> */}
                         {/* <div className="img-wrapper">
                                 <img src="/assets/images/bannerImages/banner-1.jpeg" className="d-block w-100" alt="product" />
@@ -181,7 +181,7 @@ const SliderView = () => {
                             </div>
                         </div> */}
 
-                        <div className='row bg-info-subtle'>
+                        {/* <div className='row bg-info-subtle'>
                             <div className='col-12 col-md-6'>
                                 <div className='py-5 px-3'>
                                     <p className="fs-5 fw-bold lh-1 ls-1 text-muted"><small>Hot promotions</small></p>
@@ -192,9 +192,9 @@ const SliderView = () => {
                                 </div>
                             </div>
                             <div className='col-12 col-md-6'>
-                                <img src="/assets/images/productImages/product1.png" className="d-block w-100" alt="product" />
+                                <img src="/assets/images/productImages/product1.png" className="position-absolute top-0 w-100" alt="product" />
                             </div>
-                        </div>
+                        </div> */}
 
                         {/* <div className="card-body">
                                 <h5 className="card-title">Card title 3</h5>
@@ -202,10 +202,122 @@ const SliderView = () => {
                                     card's content.</p> */}
                         {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
                         {/* </div> */}
+                    {/* </div>
+                </div> */}
+                <div className='carousel-item active'>
+                    <div className="card">
+                        <div className="bg-info-subtle px-3 px-md-5 py-3"
+                            style={{ backgroundImage: "url('/assets/images/productImages/product1.png')", backgroundPosition: "right center", backgroundRepeat: "no-repeat", backgroundSize: 'contain', visibility: "visible" }}>
+                            <div className="mt-3">
+                                <span className="h3 text-brand fw-semibold lh-1">Deals of the Day.</span><br />
+                                <p className="fs-6 fw-semibold">Limited quantities.</p>
+                            </div>
+                            <div className="">
+                                <span className="h5">
+                                    <a className="text-decoration-none text-dark" href="/" style={{ fontFamily: 'Montez' }}>
+                                        Handcrafted New Morden Design
+                                    </a>
+                                </span>
+                                <div className="my-3">
+                                    <span className="fw-bold fs-4 me-2" style={{ color: '#088178' }} >₹139.00</span>
+                                    <span className="fw-semibold fs-5 text-danger text-decoration-line-through ms-2">₹160.99</span>
+                                </div>
+                            </div>
+                            <div className="">
+                                <span>Hurry Up! Offer End In:</span>
+                                <div className="pt-2 mt-5">
+                                    <a href="/" className="btn btn-outline-dark">Shop Now <i className="bi bi-arrow-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="carousel-item">
+                <div className='carousel-item'>
                     <div className="card">
+                        <div className="bg-info-subtle px-3 px-md-5 py-3"
+                            style={{ backgroundImage: "url('/assets/images/productImages/product1.png')", backgroundPosition: "right center", backgroundRepeat: "no-repeat", backgroundSize: 'contain', visibility: "visible" }}>
+                            <div className="mt-3">
+                                <span className="h3 text-brand fw-semibold lh-1">Deals of the Day.</span><br />
+                                <p className="fs-6 fw-semibold">Limited quantities.</p>
+                            </div>
+                            <div className="">
+                                <span className="h5">
+                                    <a className="text-decoration-none text-dark" href="/" style={{ fontFamily: 'Montez' }}>
+                                        Handcrafted New Morden Design
+                                    </a>
+                                </span>
+                                <div className="my-3">
+                                    <span className="fw-bold fs-4 me-2" style={{ color: '#088178' }} >₹139.00</span>
+                                    <span className="fw-semibold fs-5 text-danger text-decoration-line-through ms-2">₹160.99</span>
+                                </div>
+                            </div>
+                            <div className="">
+                                <span>Hurry Up! Offer End In:</span>
+                                <div className="pt-2 mt-5">
+                                    <a href="/" className="btn btn-outline-dark">Shop Now <i className="bi bi-arrow-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='carousel-item'>
+                    <div className="card">
+                        <div className="bg-info-subtle px-3 px-md-5 py-3"
+                            style={{ backgroundImage: "url('/assets/images/productImages/product1.png')", backgroundPosition: "right center", backgroundRepeat: "no-repeat", backgroundSize: 'contain', visibility: "visible" }}>
+                            <div className="mt-3">
+                                <span className="h3 text-brand fw-semibold lh-1">Deals of the Day.</span><br />
+                                <p className="fs-6 fw-semibold">Limited quantities.</p>
+                            </div>
+                            <div className="">
+                                <span className="h5">
+                                    <a className="text-decoration-none text-dark" href="/" style={{ fontFamily: 'Montez' }}>
+                                        Handcrafted New Morden Design
+                                    </a>
+                                </span>
+                                <div className="my-3">
+                                    <span className="fw-bold fs-4 me-2" style={{ color: '#088178' }} >₹139.00</span>
+                                    <span className="fw-semibold fs-5 text-danger text-decoration-line-through ms-2">₹160.99</span>
+                                </div>
+                            </div>
+                            <div className="">
+                                <span>Hurry Up! Offer End In:</span>
+                                <div className="pt-2 mt-5">
+                                    <a href="/" className="btn btn-outline-dark">Shop Now <i className="bi bi-arrow-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='carousel-item'>
+                    <div className="card">
+                        <div className="bg-info-subtle px-3 px-md-5 py-3"
+                            style={{ backgroundImage: "url('/assets/images/productImages/product1.png')", backgroundPosition: "right center", backgroundRepeat: "no-repeat", backgroundSize: 'contain', visibility: "visible" }}>
+                            <div className="mt-3">
+                                <span className="h3 text-brand fw-semibold lh-1">Deals of the Day.</span><br />
+                                <p className="fs-6 fw-semibold">Limited quantities.</p>
+                            </div>
+                            <div className="">
+                                <span className="h5">
+                                    <a className="text-decoration-none text-dark" href="/" style={{ fontFamily: 'Montez' }}>
+                                        Handcrafted New Morden Design
+                                    </a>
+                                </span>
+                                <div className="my-3">
+                                    <span className="fw-bold fs-4 me-2" style={{ color: '#088178' }} >₹139.00</span>
+                                    <span className="fw-semibold fs-5 text-danger text-decoration-line-through ms-2">₹160.99</span>
+                                </div>
+                            </div>
+                            <div className="">
+                                <span>Hurry Up! Offer End In:</span>
+                                <div className="pt-2 mt-5">
+                                    <a href="/" className="btn btn-outline-dark">Shop Now <i className="bi bi-arrow-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="carousel-item">
+                    <div className="card" style={{ maxHeight: '500px' }}> */}
                         {/* <div className="img-wrapper"><img src="..." className="d-block w-100" alt="..."> </div> */}
                         {/* <div className="img-wrapper">
                                 <img src="/assets/images/bannerImages/banner-1.jpeg" className="d-block w-100" alt="product" />
@@ -223,22 +335,22 @@ const SliderView = () => {
                                 <p className="fs-6 fw-semibold">Limited quantities.</p>
                             </div>
                         </div> */}
-                        <div className='row bg-danger-subtle'>
+                        {/* <div className='row bg-danger-subtle'>
                             <div className='col-12 col-md-6'>
                                 <div className='py-5 px-3'>
                                     <p className="fs-5 fw-bold lh-1 ls-1 text-muted"><small>Trade in offer</small></p>
                                     <p className="fs-3 fw-bold lh-1">Super Value Deals</p>
-                                    <p className="fs-1 fw-bold lh-1 ls-1 text-success">on All Products</p>
+                                    <p className="fs-1 fw-bold lh-1 ls-1 text-success">On All Products</p>
                                     <p className="fs-6 fw-semibold lh-1"><small>Save more with coupons & upto 20% off</small></p>
                                     <button className='btn btn-success mt-5'>SHOP NOW</button>
                                 </div>
                             </div>
                             <div className='col-12 col-md-6'>
-                                <img src="/assets/images/productImages/product1.png" className="d-block w-100" alt="product" />
+                                <img src="/assets/images/productImages/product1.png" className="img-wrapper" alt="product" />
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
                 {/* <div className="carousel-item">
                         <div className="card"> */}
                 {/* <div className="img-wrapper"><img src="..." className="d-block w-100" alt="..."> </div> */}
@@ -274,14 +386,16 @@ const SliderView = () => {
                         </div>
                     </div> */}
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+            <div className="position-absolute end-0 translate-middle carousel_control">
+                <button className="carousel-control-prev me-5" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next ms-5" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
         // </div>
     )
