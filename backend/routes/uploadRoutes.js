cf77962da033d445267b1ee3e4b8655b36ece780
Amarendra Rout <22717048+amar-rout@users.noreply.github.com>
2023-04-25 21:35:20 +0530

    25-04-23>21:35

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 });
 
 function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
+  const filetypes = /jpg|jpeg|webp|png/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
 
