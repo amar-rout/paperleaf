1bdf3131389efc938985c913522d3c056eb9bd42
Amarendra Rout <22717048+amar-rout@users.noreply.github.com>
2023-04-30 18:45:20 +0530

    Mongodb server configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 // const env = require("dotenv")
-// env.config()
+dotenv.config();
 
 export default async function connectDB() {
   
@@ -13,11 +14,17 @@ export default async function connectDB() {
   // };
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const {MONGODB_USER, MONGODB_PASSWORD, MONGODB_URI, MONGODB_DBNAME, MONGODB_AUTH_SOURCE, MONGODB_READ_PREFERENCE} = process.env;
+    const PASSWORD = encodeURIComponent(MONGODB_PASSWORD);
+    const DB_URL = `mongodb://${MONGODB_USER}:${PASSWORD}@${MONGODB_URI}/${MONGODB_DBNAME}?authSource=${MONGODB_AUTH_SOURCE}&readPreference=${MONGODB_READ_PREFERENCE}&serverSelectionTimeoutMS=2000&ssl=false&directConnection=true`;
+    const conn = await mongoose.connect(DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
+
+    // const DB_URL = `mongodb://${DB_USER}:${PASSWORD}@89.116.230.218:27017/paperleaf_demo?retryWrites=true`;
+    // ?compressors=zlib&gssapiServiceName=mongodb
 
     console.log(`%cMongoDB Connected: ${conn.connection.host}`, 'color:green');
   } catch (error) {
