044c8c2700150643d5396668b49a74da04a725c3
Amarendra Rout <22717048+amar-rout@users.noreply.github.com>
2023-07-10 09:47:07 +0530

    m

--- frontend/src/Component/Homepage/pages/Home/HomeCarousel/HomeCarousel.js
+++ frontend/src/Component/Homepage/pages/Home/HomeCarousel/HomeCarousel.js
@@ -122,14 +122,14 @@ const HomeCarousel = () => {
                         </div>
                     </div> */}
                 </div>
-                <button className="d-none d-sm-inline carousel-control-prev " type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
+                {/* <button className="d-none d-sm-inline carousel-control-prev " type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon " aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
                 </button>
                 <button className="d-none d-sm-inline carousel-control-next " type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
                     <span className="carousel-control-next-icon " aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
-                </button>
+                </button> */}
 
 
                 {/* <button className="d-none d-sm-inline carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
