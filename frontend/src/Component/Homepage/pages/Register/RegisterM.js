c2de3a91280492bf89a8cd8811b9d2c0db4dc33e
Amarendra Rout <22717048+amar-rout@users.noreply.github.com>
2023-07-13 01:09:16 +0530

    m

--- frontend/src/Component/Homepage/pages/Register/RegisterM.js
+++ frontend/src/Component/Homepage/pages/Register/RegisterM.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const RegisterM = () => {
     return (
-        <div className='container p-2 mt-5'>
-            <dt className='display-6 '>Thanks for vising our site.</dt>
-            <dd className='text-muted'>Our website is under maintenance for which no user can register here. Please check again.</dd>
+        <div className='container p-2 mt-5 mb-5'>
+            <dt className='display-6 fw-bold mb-3'>Thanks for vising our site.</dt>
+            <dd className='fs-6 fw-normal text-muted'>We apologize for the inconvenience, but our website is currently undergoing maintenance to provide you with an improved experience. Due to high traffic, we have temporarily disabled the registration process. We appreciate your interest in joining our community and assure you that registration will be available again soon.</dd>
         </div>
     )
 }
